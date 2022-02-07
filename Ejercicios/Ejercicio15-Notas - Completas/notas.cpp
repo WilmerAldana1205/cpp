e394d642861843168795c0caff74468d5040c5e8
Wilmer Aldana <wslopza@unah.hn>
2022-02-06 18:50:29 -0600

    Correcion de tarea

--- Ejercicios/Ejercicio15-Notas - Completas/notas.cpp
+++ Ejercicios/Ejercicio15-Notas - Completas/notas.cpp
@@ -13,95 +13,95 @@ cin >> nota;
 	
     cout << "Ingrese una nota que entre 0 - 100";
     return 0;
-}
+} else
 
-if (nota >=100 && nota <=100) {
+if (nota >=96 && nota <=100) {
     cout << "Obtuviste una A++";
-}
+}else
     
 
-if (nota >=94 && nota <= 95) {
+if (nota >=91 && nota <= 95) {
     cout << "Obtuviste una A+";
     
-}
+}else
 
 if (nota >=86 && nota <= 90) {
     cout << "Obtuviste una A";
-}
+}else
 
-if (nota >=80 && nota <= 84) {
+if (nota >=81 && nota <= 85) {
     cout << "Obtuviste una A-";
     
-}
+}else
 
-if (nota >=80 && nota <= 84) {
+if (nota >=76 && nota <= 80) {
     cout << "Obtuviste una B+";
     
-}
+}else
 
-if (nota >=70 && nota <= 74) {
+if (nota >=71 && nota <= 75) {
     cout << "Obtuviste una B";
     
-}
+}else
 
 if (nota >=66 && nota <= 70) {
     cout << "Obtuviste una B-";
     
-}
+}else
 
 if (nota >=61 && nota <= 65) {
     cout << "Obtuviste una C+";
-}
+}else
     
 
 if (nota >=56 && nota <= 60) {
     cout << "Obtuviste una C";
-}
+}else
     
 
 if (nota >=51 && nota <= 55) {
     cout << "Obtuviste una C-";
-}
+}else
 
 if (nota >=46 && nota <= 50) {
     cout << "Obtuviste una D+";
-}
+}else
     
     if (nota >=41 && nota <= 45) {
     cout << "Obtuviste una D";
-}
+}else
     
     if (nota >=36 && nota <= 40) {
     cout << "Obtuviste una D-";
-}
+}else
     
     if (nota >=31 && nota <= 35) {
     cout << "Obtuviste una E+";
-}
+}else
 
   if (nota >=26 && nota <= 30) {
     cout << "Obtuviste una E";
-}
+}else
     
     if (nota >=21 && nota <= 25) {
     cout << "Obtuviste una E-";
-}
+}else
     
     if (nota >=16 && nota <= 20) {
     cout << "Obtuviste una F+";
     
-}
+}else
     if (nota >=10 && nota <= 15) {
     cout << "Obtuviste una F";
-}
+}else
 
-    if (nota >=6 && nota <= 10) {
+    if (nota >=1 && nota <= 5) {
     cout << "Obtuviste una F-";
-}
+}else
     
     if (nota >=0 && nota <=0) {
     cout << "Obtuviste una F--";
-}
+}else
 
 return 0;
 
